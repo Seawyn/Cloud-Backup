8a3adff58fbbc6b98f02349cd061d71aaf8c0a7e
Joana Silva Teodoro <ist186440@lab6p9.localdomain>
2018-10-03 17:23:02 +0100

    udp working

--- bs_test.py
+++ bs_test.py
@@ -1,11 +1,22 @@
 import socket
 import sys
+import os
+
+scktUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def child():
+    msg = "+BS: "
+    print("oi")
+    scktUDP.sendto(msg.encode(), (UDP_IP, 58017))
+    print("oiDSA")
+    print("child process.")
+    os._exit(0)
+
 
 #chamar a funçao child quando for necessario criar novo processo
 def main():
     UDP_IP = 'localhost'
     UDP_PORT = 58017
-    scktUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     users = {}
     if(len(sys.argv) == 4 and (isinstance(sys.argv[1], int))):
         CSport = input("Port: ")
@@ -17,10 +28,25 @@ def main():
 
     scktUDP.bind((UDP_IP, UDP_PORT))
 
+    data, addr = scktUDP.recvfrom(1024) # buffer size is 1024 bytes
+    ola = 0
+    while ola < 2:
+        if data:
+            print (data.decode())
+            data = 0
+        else:
+            #child()
+            msg = "+BS: "
+            print(addr)
+            scktUDP.sendto(msg.encode(), addr)
+            print("mandei msg")
+        ola += 1
+        '''if os.fork() == 0:
+                print("oi")
+                child()
+            else:
+                print("entrei no else")'''
 
-    while True:
-        data, addr = scktUDP.recvfrom(1024) # buffer size is 1024 bytes
-        print (data.decode())
     return 0
 
 
--- cs_test.py
+++ cs_test.py
@@ -5,12 +5,21 @@
 
 import socket
 import sys
+import os
 
+scktUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+def child():
+    data, addr = scktUDP.recvfrom(1024) # the problem is here
+    print(data)
+    if data:
+        print (data.decode())
+    else:
+        os._exit(0)
 
 def main():
     UDP_IP = 'localhost'
 
-    scktUDP = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     users = {}
     if(len(sys.argv) == 3 and (isinstance(sys.argv[2], int))):
         CSport = input("Port: ")
@@ -20,7 +29,19 @@ def main():
         CSport = 58017
         BSport = 59000
     a = "RGR OK\n"
+
     scktUDP.sendto(a.encode(), (UDP_IP, 58017))
+    '''if os.fork() == 0:
+        child()
+        print("child process.")
+    else:
+        print("entrei no else")'''
+    print("vou receber")
+    data, addr = scktUDP.recvfrom(1024) # the problem is here
+    print("recebi")
+    if data:
+        print (data.decode())
+
 
     return 0
 
