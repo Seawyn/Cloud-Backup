f4ab87972aad481d09a0122a71db3b2d5447cf70
Seawyn <32650833+Seawyn@users.noreply.github.com>
2018-10-08 18:17:11 +0100

    Add files via upload

--- BCK_Server.py
+++ BCK_Server.py
@@ -1,50 +1,5 @@
 import socket
 import os
-<<<<<<< HEAD
-import time
-
-def send_file(folder, filename, sock):
-   path = os.path.join(folder, filename)
-   stat = time.gmtime(os.path.getmtime(path))
-   time1 = time.strftime('%d.%m.%y', stat)
-   time2 = time.strftime('%H:%M:%S', stat)
-   size = os.path.getsize(path)
-   Message = filename + ' ' + time1 + ' ' + time2 + ' ' + str(size)
-   sock.send(Message.encode())
-   f = open(path, 'rb')
-   bytesToSend = f.read(1024)
-   while(bytesToSend):
-       sock.send(bytesToSend)
-       bytesToSend = f.read(1024)
-   f.close()
-
-def main():
-   TCP_IP = socket.gethostbyname(socket.gethostname())
-   TCP_PORT = 58017
-
-   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-   sock.connect((TCP_IP, TCP_PORT))
-
-   user = input("User: ")
-   sock.send(user.encode())
-   Message = sock.recv(1024)
-   print("Server: ", Message.decode())
-
-   name = input("Which folder do you want to send?\n")
-   files = os.listdir(name)
-   num = len(files)
-   Message = str(num)
-   sock.send(Message.encode())
-   sock.recv(1024)
-
-   for i in range(num):
-       send_file(name, files[i], sock)
-       data = sock.recv(1024)
-       print(data.decode())
-   sock.close()
-
-=======
 
 def receive_file(user, sock, no_files):
     for i in range(int(no_files)):
@@ -100,5 +55,4 @@ def main():
     receive_file(user, conn, data)
     conn.close()
     
->>>>>>> 1b0faa3bd6f2e9c3e01bb313246b82015c145a51
 main()
